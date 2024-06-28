971b0b63c7fc9ec9fd454b8c902cd95999cd0353
kevin <kipronok234@gmail.com>
2024-06-28 14:01:05 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const receivesAFunction = (callback) => {
+    callback();
+}
+const returnsANamedFunction = () => {
+    function namedFunction() {
+
+    }
+    return namedFunction;
+}
+const returnsAnAnonymousFunction = () => {
+    return function(){
+        
+    }
+}
